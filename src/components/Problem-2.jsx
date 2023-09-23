7d71cc79ea3c096f9d38d9dd5ad068f0a0cdf59a
nahid-dev <nahidv232@gmail.com>
2023-09-23 22:01:25 +0600

    create the modal and fetch all contact data

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,21 +1,98 @@
-import React from 'react';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const Problem2 = () => {
+  const [showModalA, setShowModalA] = useState(false);
+  const [showModalB, setShowModalB] = useState(false);
+  const [contacts, setContacts] = useState([]);
+  const [onlyEven, setOnlyEven] = useState(false);
 
-    return (
+  // Function to fetch contacts from the API
+  const fetchContacts = async () => {
+    try {
+      // Fetch contacts from the API based on the selected category (All or US)
+      // Update the 'contacts' state with the fetched data
+    } catch (error) {
+      console.error("Error fetching contacts:", error);
+    }
+  };
 
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
-                <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
-                </div>
-                
-            </div>
+  // Function to toggle between Modals A and B
+  const toggleModalA = () => {
+    setShowModalA(true);
+    setShowModalB(false);
+  };
+
+  const toggleModalB = () => {
+    setShowModalA(false);
+    setShowModalB(true);
+  };
+
+  // Function to close both modals
+  const closeModal = () => {
+    setShowModalA(false);
+    setShowModalB(false);
+  };
+
+  // Function to toggle the "Only even" checkbox
+  const handleCheckboxChange = () => {
+    setOnlyEven(!onlyEven);
+  };
+
+  // Fetch contacts when the component mounts (you might want to use useEffect here)
+  useEffect(() => {
+    fetch("https://contact.mediusware.com/api/contacts/")
+      .then((res) => res.json())
+      .then((data) => setContacts(data.results));
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
+
+        <div className="d-flex justify-content-center gap-3">
+          <button
+            className="btn btn-lg btn-outline-primary"
+            type="button"
+            onClick={toggleModalA}
+          >
+            All Contacts
+          </button>
+          <button
+            className="btn btn-lg btn-outline-warning"
+            type="button"
+            onClick={toggleModalB}
+          >
+            US Contacts
+          </button>
         </div>
-    );
+      </div>
+
+      {showModalA && (
+        <div className="border mt-5 p-5">
+          <div>
+            <h2 className="display-4">Modal A</h2>
+          </div>
+          <button className="btn me-5 btn-primary">All Contact</button>
+          <button className="btn me-5 btn-secondary">US Contact</button>
+          <button className="btn me-5 btn-warning">Close</button>
+        </div>
+      )}
+
+      {showModalB && (
+        <div className="border mt-5 p-5">
+          <div>
+            <h2 className="display-4">Modal B</h2>
+          </div>
+          <button className="btn me-5 btn-primary">All Contact</button>
+          <button className="btn me-5 btn-secondary">US Contact</button>
+          <button className="btn me-5 btn-warning">Close</button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Problem2;
